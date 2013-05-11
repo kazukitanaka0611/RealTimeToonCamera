a981b19b7dd4fb3e450780be6a8327eaf730426f
kazuki_tanaka <zukeyny@gmail.com>
2013-05-11 09:06:21 +0900

    Initial Commit

---
+++ RealTimeToonCamera/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RealTimeToonCamera
+//
+//  Created by kazuki_tanaka on 2013/05/11.
+//  Copyright (c) 2013年 kazukitanaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
